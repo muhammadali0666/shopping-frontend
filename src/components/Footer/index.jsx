a9d7f10f1a8f22b94e223e397b6b818aa88f53d7
muhammadali0666 <muhammadalishuhratjonov50@gmail.com>
2023-08-11 19:27:09 +0500

    added some section

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,294 @@
-import "./footer.css"
+import "./footer.css";
+import MiniWhatsap from "../../assets/img/s.png";
+import MiniTelegram from "../../assets/img/M.png";
+import Inst from "../../assets/img/instagram.png";
+import Facebook from "../../assets/img/facebook.png";
+import Whatsapp from "../../assets/img/whatsap.png";
+import Youtube from "../../assets/img/youtube.png";
+import Telegram from "../../assets/img/telegram.png";
+import Linkedin from "../../assets/img/linkedin.png";
+import Send from "../../assets/img/send.png";
+import FooterLogo from "../../assets/img/footer_logo.png"
+import Visa from "../../assets/img/visa.png"
+import Master from "../../assets/img/mastercard white.png"
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer">
+      <div className="container">
+        <div className="footer_inner">
+          <div className="footer_wrapper">
+            <ul className="footer_list">
+              <li className="footer_item">
+                <h4 href="#" className="footer_item_paragraph">
+                  Информация
+                </h4>
+              </li>
+              <li className="footer_item">
+                <a href="#" className="footer_item_link">
+                  Акции
+                </a>
+              </li>
+              <li className="footer_item">
+                <a href="#" className="footer_item_link">
+                  Оплата и доставка
+                </a>
+              </li>
+              <li className="footer_item">
+                <a href="#" className="footer_item_link">
+                  Гарантия
+                </a>
+              </li>
+              <li className="footer_item">
+                <a href="#" className="footer_item_link">
+                  Частые вопросы
+                </a>
+              </li>
+              <li className="footer_item">
+                <a href="#" className="footer_item_link">
+                  Новости
+                </a>
+              </li>
+              <li className="footer_item">
+                <a href="#" className="footer_item_link">
+                  Блог
+                </a>
+              </li>
+              <li className="footer_item">
+                <a href="#" className="footer_item_link">
+                  О нас
+                </a>
+              </li>
+              <li className="footer_item">
+                <a href="#" className="footer_item_link">
+                  Политика конфиденциальности
+                </a>
+              </li>
+              <li className="footer_item">
+                <a href="#" className="footer_item_link">
+                  Контакты
+                </a>
+              </li>
+            </ul>
+            <ul className="footer_list">
+              <li className="footer_item">
+                <h4 href="#" className="footer_item_paragraph">
+                  Услуги и сервисы
+                </h4>
+              </li>
+              <li className="footer_item">
+                <a href="#" className="footer_item_link">
+                  Сервисный центр v-comp
+                </a>
+              </li>
+              <li className="footer_item">
+                <a href="#" className="footer_item_link">
+                  Магазин Б/У товара
+                </a>
+              </li>
+              <li className="footer_item">
+                <a href="#" className="footer_item_link">
+                  Скупка Б/У
+                </a>
+              </li>
+              <li className="footer_item">
+                <a href="#" className="footer_item_link">
+                  Ремонт ПК и оргтехники
+                </a>
+              </li>
+              <li className="footer_item">
+                <a href="#" className="footer_item_link">
+                  Компьютерная помощь
+                </a>
+              </li>
+              <li className="footer_item">
+                <a href="#" className="footer_item_link">
+                  Сотрудничество
+                </a>
+              </li>
+              <li className="footer_item">
+                <a href="#" className="footer_item_link">
+                  Главная
+                </a>
+              </li>
+            </ul>
+            <ul className="footer_list">
+              <li className="footer_item">
+                <h4 href="#" className="footer_item_paragraph">
+                  Контакты
+                </h4>
+              </li>
+              <li className="footer_item">
+                <a href="#" className="footer_item_link">
+                  (067) 11-12-485 - Отдел продаж
+                </a>
+              </li>
+              <li className="footer_item">
+                <a href="#" className="footer_item_link">
+                  (066) 484-39-22 - Отдел продаж
+                </a>
+              </li>
+              <li className="footer_item">
+                <a href="#" className="footer_item_link">
+                  (063) 747-33-48 - Отдел продаж
+                  <img
+                    src={MiniWhatsap}
+                    alt="icon"
+                    className="footer_item_link_img"
+                    width={12}
+                    height={12}
+                  />
+                  <img
+                    src={MiniTelegram}
+                    alt="icon"
+                    className="footer_item_link_img"
+                    width={12}
+                    height={12}
+                  />
+                </a>
+              </li>
+              <li className="footer_item">
+                <a href="#" className="footer_item_link">
+                  Днепр Европейская, 8 (бывшая Миронова 8)
+                </a>
+              </li>
+              <li className="footer_item">
+                <a href="#" className="footer_item_link">
+                  Понедельник-Пятница 9:00-19:00 Суббота-Воскресенье: с
+                  9:00-16:00
+                </a>
+              </li>
+            </ul>
+            <div className="footer_box">
+              <h3 className="footer_box_paragraph">Следите за нами</h3>
+              <ul className="footer_box_list">
+                <li className="footer_box_item">
+                  <a href="#" className="footer_box_link">
+                    <img
+                      src={Inst}
+                      alt=""
+                      className="footer_box_img"
+                      width={31}
+                      height={32}
+                    />
+                  </a>
+                </li>
+                <li className="footer_box_item">
+                  <a href="#" className="footer_box_link">
+                    <img
+                      src={Whatsapp}
+                      alt=""
+                      className="footer_box_img"
+                      width={31}
+                      height={32}
+                    />
+                  </a>
+                </li>
+                <li className="footer_box_item">
+                  <a href="#" className="footer_box_link">
+                    <img
+                      src={Telegram}
+                      alt=""
+                      className="footer_box_img"
+                      width={31}
+                      height={32}
+                    />
+                  </a>
+                </li>
+                <li className="footer_box_item">
+                  <a href="#" className="footer_box_link">
+                    <img
+                      src={Youtube}
+                      alt=""
+                      className="footer_box_img"
+                      width={31}
+                      height={32}
+                    />
+                  </a>
+                </li>
+                <li className="footer_box_item">
+                  <a href="#" className="footer_box_link">
+                    <img
+                      src={Linkedin}
+                      alt=""
+                      className="footer_box_img"
+                      width={31}
+                      height={32}
+                    />
+                  </a>
+                </li>
+                <li className="footer_box_item">
+                  <a href="#" className="footer_box_link">
+                    <img
+                      src={Facebook}
+                      alt=""
+                      className="footer_box_img"
+                      width={31}
+                      height={32}
+                    />
+                  </a>
+                </li>
+              </ul>
+              <h4 className="footer_box_paragraph">Подписывайтесь на скидки</h4>
+              <form className="footer_box_form">
+                <input
+                  type="text"
+                  className="footer_box_input"
+                  placeholder="Укажите ваш email..."
+                />
+                <button className="footer_box_btn">
+                  <img
+                    src={Send}
+                    alt=""
+                    className="footer_box_btn_img"
+                    width={32}
+                    height={32}
+                  />
+                </button>
+              </form>
+            </div>
+          </div>
+          <div className="footer_bottom">
+            <ul className="footer_bottom_list">
+              <li className="footer_bottom_item">
+                <a href="#" className="footer_logo_link">
+                  <img
+                    src={FooterLogo}
+                    alt="icon"
+                    className="footer_logo"
+                    width={159}
+                    height={40}
+                  />
+                </a>
+                <p className="footer_bottom_p">
+                  {new Date().getFullYear()} Интернет-магазин v-comp.com.ua Все
+                  права защищены
+                </p>
+              </li>
+              <li className="footer_bottom_item">
+                <a href="#" className="footer_logo_link">
+                  <img
+                    src={Master}
+                    alt="icon"
+                    className="footer_logo"
+                    width={50}
+                    height={31}
+                  />
+                </a>
+                <a href="#" className="footer_logo_link">
+                  <img
+                    src={Visa}
+                    alt="icon"
+                    className="footer_logo"
+                    width={50}
+                    height={32}
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,81 +1,99 @@
 import "./header.css";
 import Hamburger from "../../assets/img/hamburger.png";
 import User from "../../assets/img/user.png";
-import Logo from "../../assets/img/logo.png"
-import Katalog from "../../assets/img/Catalog.png"
-import Like from "../../assets/img/like.png"
-import Notification from "../../assets/img/notification.png"
-import Shopping from "../../assets/img/shopping.png"
-import Search from "../../assets/img/search.png"
+import Logo from "../../assets/img/logo.png";
+import Katalog from "../../assets/img/Catalog.png";
+import Like from "../../assets/img/like.png";
+import Notification from "../../assets/img/notification.png";
+import Shopping from "../../assets/img/shopping.png";
+import Search from "../../assets/img/search.png";
 
 export const Header = () => {
   return (
     <div className="header">
+      <div className="header_top">
+        <div className="container header_top_inner">
+          <ul className="header_top_list">
+            <li className="header_top_item">
+              <img src={Hamburger} alt="asdas" width={20} height={20} />
+            </li>
+            <li className="header_top_item">
+              <a href="#" className="header_top_item_link">
+                Акции
+              </a>
+            </li>
+            <li className="header_top_item">
+              <a href="#" className="header_top_item_link">
+                Оплата и доставка
+              </a>
+            </li>
+            <li className="header_top_item">
+              <a href="#" className="header_top_item_link">
+                Помощь
+              </a>
+            </li>
+            <li className="header_top_item">
+              <a href="#" className="header_top_item_link">
+                Скупка Б/У
+              </a>
+            </li>
+            <li className="header_top_item">
+              <a href="#" className="header_top_item_link">
+                Контакты
+              </a>
+            </li>
+          </ul>
+          <ul className="header_top_list_next">
+            <li className="header_top_item_next">
+              <a href="#" className="header_top_item_link_next">
+                Uz
+              </a>
+              <a href="#" className="header_top_item_link_next">
+                /
+              </a>
+              <a href="#" className="header_top_item_link_next">
+                Eng
+              </a>
+            </li>
+            <li className="header_top_item_next">
+              <a href="#" className="header_top_item_link_next">
+                <img src={User} alt="asdas" width={32} height={32} />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
       <div className="container">
         <div className="header_inner">
-          <div className="header_top">
-            <ul className="header_top_list">
-              <li className="header_top_item">
-                <img src={Hamburger} alt="asdas" width={20} height={20} />
-              </li>
-              <li className="header_top_item">
-                <a href="#" className="header_top_item_link">
-                  Акции
-                </a>
-              </li>
-              <li className="header_top_item">
-                <a href="#" className="header_top_item_link">
-                  Оплата и доставка
-                </a>
-              </li>
-              <li className="header_top_item">
-                <a href="#" className="header_top_item_link">
-                  Помощь
-                </a>
-              </li>
-              <li className="header_top_item">
-                <a href="#" className="header_top_item_link">
-                  Скупка Б/У
-                </a>
-              </li>
-              <li className="header_top_item">
-                <a href="#" className="header_top_item_link">
-                  Контакты
-                </a>
-              </li>
-            </ul>
-            <ul className="header_top_list_next">
-              <li className="header_top_item_next">
-                <img src={Hamburger} alt="asdas" width={20} height={20} />
-              </li>
-              <li className="header_top_item_next">
-                <a href="#" className="header_top_item_link_next">
-                  Uz
-                </a>
-                <a href="#" className="header_top_item_link_next">
-                  Eng
-                </a>
-              </li>
-              <li className="header_top_item_next">
-                <a href="#" className="header_top_item_link_next">
-                  <img src={User} alt="asdas" width={32} height={32} />
-                </a>
-              </li>
-            </ul>
-          </div>
           {/* header_bottom  */}
           <div className="header_bottom">
             <a href="#" className="header_logo_link">
-              <img src="" alt="" className="header_logo" />
+              <img src={Logo} alt="" className="header_logo" width={164} height={42}/>
             </a>
             <button className="header_btn">
-            КАТАЛОГ ТОВАРОВ
-            <img src={Katalog} alt="katalog" className="katalog_img" width={24} height={24}/>
+              КАТАЛОГ ТОВАРОВ
+              <img
+                src={Katalog}
+                alt="katalog"
+                className="katalog_img"
+                width={24}
+                height={24}
+              />
             </button>
             <form className="header_form">
-              <input type="text" className="header_input" placeholder="search..."/>
+              <input
+                type="text"
+                className="header_input"
+                placeholder="search..."
+              />
               <button className="header_search_btn">
-                <img src={Search} alt="search" className="header_btn_img" width={24} height={24}/>
+                <img
+                  src={Search}
+                  alt="search"
+                  className="header_btn_img"
+                  width={24}
+                  height={24}
+                />
               </button>
             </form>
             <select className="header_select">
@@ -85,13 +103,31 @@ export const Header = () => {
             </select>
             <ul className="header_icons_list">
               <li className="header_icons_item">
-                <img src={Notification} alt="" className="header_icons_img" width={46} height={49}/>
+                <img
+                  src={Notification}
+                  alt=""
+                  className="header_icons_img"
+                  width={46}
+                  height={49}
+                />
               </li>
               <li className="header_icons_item">
-                <img src={Like} alt="" className="header_icons_img" width={46} height={49}/>
+                <img
+                  src={Like}
+                  alt=""
+                  className="header_icons_img"
+                  width={46}
+                  height={49}
+                />
               </li>
               <li className="header_icons_item">
-                <img src={Shopping} alt="" className="header_icons_img" width={46} height={49}/>
+                <img
+                  src={Shopping}
+                  alt=""
+                  className="header_icons_img"
+                  width={46}
+                  height={49}
+                />
               </li>
             </ul>
           </div>
