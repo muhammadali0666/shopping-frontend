fb57d764c6214c52f624b43c36344f4014610801
muhammadali0666 <muhammadalishuhratjonov50@gmail.com>
2023-08-27 15:17:14 +0500

    added contact page

--- src/components/ContactComp/index.jsx
+++ src/components/ContactComp/index.jsx
@@ -16,7 +16,11 @@ export const ContactComp = () => {
             <div className="contact_left_box">
               <ul className="footer_list">
                 <li className="footer_item">
-                  <a href="#" className="footer_item_link">
+                  <a
+                    target="_blank"
+                    href="tel:+998904565025"
+                    className="footer_item_link"
+                  >
                     <img
                       src={Contact}
                       alt="icon"
@@ -28,7 +32,11 @@ export const ContactComp = () => {
                   </a>
                 </li>
                 <li className="footer_item">
-                  <a href="#" className="footer_item_link">
+                  <a
+                    target="_blank"
+                    href="https://wa.me/904565025"
+                    className="footer_item_link"
+                  >
                     <img
                       src={Whatsap}
                       alt="icon"
@@ -40,7 +48,11 @@ export const ContactComp = () => {
                   </a>
                 </li>
                 <li className="footer_item">
-                  <a href="#" className="footer_item_link">
+                  <a
+                    target="_blank"
+                    href="https://t.me/Sh_Muhammadali5"
+                    className="footer_item_link"
+                  >
                     <img
                       src={Telegram}
                       alt="icon"
@@ -52,7 +64,11 @@ export const ContactComp = () => {
                   </a>
                 </li>
                 <li className="footer_item">
-                  <a href="#" className="footer_item_link">
+                  <a
+                    target="_blank"
+                    href="mailto:muhammadalishuhratjono50@gmail.com"
+                    className="footer_item_link"
+                  >
                     <img
                       src={ContactMessage}
                       alt="icon"
@@ -75,21 +91,25 @@ export const ContactComp = () => {
                       height={24}
                     />
                     <p className="footer_item_link_p">
-                      Понедельник-Пятница 9:00-19:00
+                      Dushanba-Shanba 9:00-19:00
                     </p>
                   </a>
                 </li>
                 <li className="footer_item">
-                  <a href="#" className="footer_item_link">
+                  <a
+                    target="_blank"
+                    href="https://goo.gl/maps/CJPpfDDB8zSrW2cf6"
+                    className="footer_item_link"
+                  >
                     <img
                       src={ContactLocation}
                       alt="icon"
                       className="footer_item_link_img"
-                      width={24}
+                      width={18}
                       height={24}
                     />
                     <p className="footer_item_link_p">
-                      г. Днепр, ул. Европейская, 8 (бывшая Миронова 8)
+                      V. Samarqand k. Jartepa.
                     </p>
                   </a>
                 </li>
@@ -97,6 +117,7 @@ export const ContactComp = () => {
             </div>
             <div className="contact_right_box">
               <iframe
+                className="iframe"
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d196672.41028460156!2d66.67008409453128!3d39.627059500000016!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f4d1f6f36d8e657%3A0xfbe0eb224147d0a3!2sJartepa!5e0!3m2!1sru!2s!4v1692967174058!5m2!1sru!2s"
                 style={{ border: "0" }}
               ></iframe>
