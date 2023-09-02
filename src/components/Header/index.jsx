bdf4e468d844ac2baa2918224da67e94772a1fa4
muhammadali0666 <muhammadalishuhratjonov50@gmail.com>
2023-09-02 19:09:13 +0500

    added modal component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,24 +2,15 @@ import "./header.css";
 import Home from "../../assets/img/home.png";
 import User from "../../assets/img/user.png";
 import Logo from "../../assets/img/logo-store.png";
-import Katalog from "../../assets/img/Catalog.png";
+import KatalogImg from "../../assets/img/Catalog.png";
 import Like from "../../assets/img/like.png";
 import Shopping from "../../assets/img/shopping-cart.png";
 import Search from "../../assets/img/search.png";
 import { NavLink } from "react-router-dom";
 import Modal from 'react-bootstrap/Modal';
 import { useState } from "react";
-import Right from "../../assets/img/right.png";
-import Proccessor from "../../assets/img/Processor.png";
-import Monitor from "../../assets/img/monitor.png"
-import Kampitur from "../../assets/img/komiputer.png"
-import Laptop from "../../assets/img/laptop.png"
-import Controller from "../../assets/img/controller.png"
-import One from "../../assets/img/1.png"
-import Two from "../../assets/img/2.png"
-import Three from "../../assets/img/3.png"
-import Four from "../../assets/img/4.png"
-import Five from "../../assets/img/5.png"
+import { Menyu } from "../Menyu";
+import { Katalog } from "../Katalog";
 
 export const Header = () => {
   const [lgShow, setLgShow] = useState(false)
@@ -86,6 +77,8 @@ export const Header = () => {
         show={lgShow}
         onHide={() => setLgShow(false)}
         aria-labelledby="example-modal-sizes-title-lg"
+        className="modal"
+        // style={{height: "100vh", overflowY: "scroll"}}
       >
         <Modal.Header closeButton>
           <Modal.Title id="example-modal-sizes-title-lg">
@@ -93,200 +86,11 @@ export const Header = () => {
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-        <div className="menyu">
-      <ul className="menyu_list">
-        <li className="menyu_item">
-          <NavLink to={'/com_components'} className="menyu_box">
-            <img
-              src={Proccessor}
-              alt="icon"
-              className="menyu_img"
-              width={20}
-              height={20}
-            />
-            <p className="menyu_p">Kompyuter komponentlari</p>
-          </NavLink>
-          <img
-            src={Right}
-            alt="motion"
-            className="menyu_img_next"
-            width={24}
-            height={24}
-          />
-        </li>
-        <li className="menyu_item">
-          <NavLink to={'/monitor'} className="menyu_box">
-            <img
-              src={Monitor}
-              alt="icon"
-              className="menyu_img"
-              width={20}
-              height={24}
-            />
-            <p className="menyu_p">Моnitorlar</p>
-          </NavLink>
-          <img
-            src={Right}
-            alt="motion"
-            className="menyu_img_next"
-            width={24}
-            height={24}
-          />
-        </li>
-        <li className="menyu_item">
-          <NavLink to={'/computer'} className="menyu_box">
-            <img
-              src={Kampitur}
-              alt="icon"
-              className="menyu_img"
-              width={24}
-              height={24}
-            />
-            <p className="menyu_p">Kompyuterlar</p>
-          </NavLink>
-          <img
-            src={Right}
-            alt="motion"
-            className="menyu_img_next"
-            width={24}
-            height={24}
-          />
-        </li>
-        <li className="menyu_item">
-          <NavLink to={'/laptop'} className="menyu_box">
-            <img
-              src={Laptop}
-              alt="icon"
-              className="menyu_img"
-              width={16}
-              height={24}
-            />
-            <p className="menyu_p">Noutbuklar</p>
-          </NavLink>
-          <img
-            src={Right}
-            alt="motion"
-            className="menyu_img_next"
-            width={24}
-            height={24}
-          />
-        </li>
-        <li className="menyu_item">
-          <NavLink to={'/consol'} className="menyu_box">
-            <img
-              src={Controller}
-              alt="icon"
-              className="menyu_img"
-              width={20}
-              height={20}
-            />
-            <p className="menyu_p">O'yin konsollari</p>
-          </NavLink>
-          <img
-            src={Right}
-            alt="motion"
-            className="menyu_img_next"
-            width={24}
-            height={24}
-          />
-        </li>
-        <li className="menyu_item">
-          <NavLink to={'acsessuar'} className="menyu_box">
-            <img
-              src={One}
-              alt="icon"
-              className="menyu_img"
-              width={24}
-              height={12}
-            />
-            <p className="menyu_p">Noutbuklar uchun aksessuarlar</p>
-          </NavLink>
-          <img
-            src={Right}
-            alt="motion"
-            className="menyu_img_next"
-            width={24}
-            height={24}
-          />
-        </li>
-        <li className="menyu_item">
-          <NavLink to={'/planshet'} className="menyu_box">
-            <img
-              src={Two}
-              alt="icon"
-              className="menyu_img"
-              width={20}
-              height={20}
-            />
-            <p className="menyu_p">Planshetlar</p>
-          </NavLink>
-          <img
-            src={Right}
-            alt="motion"
-            className="menyu_img_next"
-            width={24}
-            height={24}
-          />
-        </li>
-        <li className="menyu_item">
-          <NavLink to={'/printer'} className="menyu_box">
-            <img
-              src={Three}
-              alt="icon"
-              className="menyu_img"
-              width={18}
-              height={24}
-            />
-            <p className="menyu_p">Printerlar</p>
-          </NavLink>
-          <img
-            src={Right}
-            alt="motion"
-            className="menyu_img_next"
-            width={24}
-            height={24}
-          />
-        </li>
-        <li className="menyu_item">
-          <NavLink to={'/kalonka'} className="menyu_box">
-            <img
-              src={Four}
-              alt="icon"
-              className="menyu_img"
-              width={20}
-              height={20}
-            />
-            <p className="menyu_p">Kalonkalar</p>
-          </NavLink>
-          <img
-            src={Right}
-            alt="motion"
-            className="menyu_img_next"
-            width={24}
-            height={24}
-          />
-        </li>
-        <li className="menyu_item">
-          <NavLink to={'/wifi'} className="menyu_box">
-            <img
-              src={Five}
-              alt="icon"
-              className="menyu_img"
-              width={24}
-              height={23}
-            />
-            <p className="menyu_p">tarmoq apparati</p>
-          </NavLink>
-          <img
-            src={Right}
-            alt="motion"
-            className="menyu_img_next"
-            width={24}
-            height={24}
-          />
-        </li>
-      </ul>
-    </div>
+          <div className="katalog_wrapper"         style={{height: "60vh", overflowY: "scroll"}}
+>
+       <Menyu/>
+       <Katalog/>
+          </div>
         </Modal.Body>
       </Modal>
 
@@ -302,7 +106,7 @@ export const Header = () => {
             <button className="header_btn" onClick={() => setLgShow(true)}>
               Mahsulot turlari
               <img
-                src={Katalog}
+                src={KatalogImg}
                 alt="katalog"
                 className="katalog_img"
                 width={24}
---
+++ src/components/Katalog/index.jsx
@@ -0,0 +1,169 @@
+import { NavLink } from "react-router-dom";
+import "./katalog.css";
+
+export const Katalog = () => {
+  return (
+    <div className="katalog">
+      <div className="katalog_left">
+        <ul className="katalog_list">
+          <li className="katalog_item">
+            <h4 className="katalog_paragrph">Noutbuklar</h4>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">MacBook</NavLink>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">Asus</NavLink>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">Lenovo</NavLink>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">Hp</NavLink>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">Dell</NavLink>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">Acer</NavLink>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">Sony</NavLink>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">Samsung</NavLink>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">Toshiba</NavLink>
+          </li>
+        </ul>
+        <ul className="katalog_list">
+          <li className="katalog_item">
+            <h4 className="katalog_paragrph">Sichqonchalar</h4>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">Logitech</NavLink>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">Razer</NavLink>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">Microsoft</NavLink>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">SteelSeries</NavLink>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">ROCCAT</NavLink>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">Asus</NavLink>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">HyperX</NavLink>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">Corsair</NavLink>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">Leovo</NavLink>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">Zowie by BenQ</NavLink>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">MSI</NavLink>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">Redroger</NavLink>
+          </li>
+        </ul>
+      </div>
+      <div className="katalog_middle">
+        <ul className="katalog_list">
+          <li className="katalog_item">
+            <h4 className="katalog_paragrph">Monitorlar</h4>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">Acer</NavLink>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">Asus</NavLink>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">Dell</NavLink>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">Gigabayte</NavLink>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">LG</NavLink>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">MSI</NavLink>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">Samsung</NavLink>
+          </li>
+        </ul>
+        <ul className="katalog_list">
+          <li className="katalog_item">
+            <h4 className="katalog_paragrph">Printerlar</h4>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">Hp</NavLink>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">Canon</NavLink>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">Xerox</NavLink>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">Brother</NavLink>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">Epson</NavLink>
+          </li>
+        </ul>
+      </div>
+      <div className="katalog_right">
+        <ul className="katalog_list">
+          <li className="katalog_item">
+            <h4 className="katalog_paragrph">Wi-Fi routerlar</h4>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">TP-Link</NavLink>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">Tenda</NavLink>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">Netis</NavLink>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">MicroTik</NavLink>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">Huawei</NavLink>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">Xiaomi</NavLink>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">Cisco</NavLink>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">Siklu</NavLink>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">MERCUSYS</NavLink>
+          </li>
+          <li className="katalog_item">
+            <NavLink className="katalog_link">SNR</NavLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
