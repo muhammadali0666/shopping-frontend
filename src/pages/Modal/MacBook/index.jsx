9b6183a738d871865fc419a28a552585702cf234
muhammadali0666 <muhammadalishuhratjonov50@gmail.com>
2023-09-19 16:52:56 +0500

    divided to category

--- src/pages/Modal/MacBook/index.jsx
+++ src/pages/Modal/MacBook/index.jsx
@@ -1,29 +1,63 @@
-import React from 'react'
-import { Header } from '../../../components/Header'
-import {Footer} from "../../../components/Footer"
-import {Card} from "../../../components/Card"
-import "./style.css"
+import React from "react";
+import { Header } from "../../../components/Header";
+import { Footer } from "../../../components/Footer";
+import { useState, useEffect } from "react";
+import Monitor from "../../../assets/img/card-monitor.png";
+import Like from "../../../assets/img/card-like.png";
+import "./style.css";
 
 export const MacBook = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:4001/get_noutbooks`)
+      .then((res) => res.json(res))
+      .then((datas) => setData(datas));
+  }, []);
   return (
-    <div className='macbook_page'>
-      <Header/>
+    <div className="macbook_page">
+      <Header />
       <div className="macbook">
         <div className="container">
           <div className="macbook_inner">
-          <h2 className="macbook_paragraph">
-          MacBook
-        </h2>
-        <ul className="macbook_list">
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-        </ul>
+            <h2 className="macbook_paragraph">MacBook</h2>
+            <ul className="macbook_list">
+              {data.length &&
+                data.map((element) =>
+                  element.brand === "MacBook" ? (
+                    <li className="card_item">
+                      <span className="card_span">
+                        <img
+                          src={Like}
+                          alt="icon"
+                          className="card_mini_img"
+                          width={24}
+                          height={24}
+                        />
+                      </span>
+                      <img
+                        src={Monitor}
+                        alt="icon"
+                        className="card_img"
+                        width={218}
+                        height={214}
+                      />
+                      <p className="card_p">{element.title}</p>
+                      <p className="card_text">Sharhlar: 0</p>
+                      <div className="card_box">
+                        <p className="card_price">{element.price}</p>
+                        <button className="card_btn btn btn-success">
+                          Sotib olish
+                        </button>
+                      </div>
+                    </li>
+                  ) : null
+                )}
+            </ul>
           </div>
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
-  )
-}
+  );
+};
